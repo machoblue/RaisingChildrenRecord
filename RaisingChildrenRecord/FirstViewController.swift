1c2e7fed99f1bc800ddb87f5539d92b95293c154
松島勇貴 <yuukimatsushima@matsushimayuukinomacbook-ea.local>
2018-07-12 08:36:26 +0900

    Initial Commit

---
+++ RaisingChildrenRecord/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  RaisingChildrenRecord
+//
+//  Created by 松島勇貴 on 2018/07/12.
+//  Copyright © 2018年 松島勇貴. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
