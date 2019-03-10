db1e33e06e08a76e2da9734a0f92846a493ec429
松島勇貴 <macho.macho.dev@gmail.com>
2019-03-10 11:15:16 +0900

    Refactor RecordObserverRealm

--- Shared/RecordObserverRealm.swift
+++ Shared/RecordObserverRealm.swift
@@ -17,7 +17,6 @@ public class RecordObserverRealm: RecordObserver {
     var realm: Realm!
     var results: Results<Record>!
     var notificationToken: NotificationToken?
-    var records: [RecordModel] = []
 
     private init() {
     }
@@ -28,38 +27,30 @@ public class RecordObserverRealm: RecordObserver {
     
     public func observe(babyId: String, from: Date, to: Date, with callback: @escaping ([(RecordModel, Change)]) -> Void) {
         notificationToken?.invalidate()
-        records = []
         realm = try! Realm()
         
         self.results = realm.objects(Record.self).filter("babyId == %@ AND %@ <= dateTime AND dateTime <= %@", babyId, from ,to)
-        for record in results {
-            self.records.append(RecordModel(from: record))
-        }
         
         notificationToken = self.results.observe { [weak self] (changes: RealmCollectionChange) in
             var myChanges: [(RecordModel, Change)] = []
             switch changes {
             case .initial:
-                for record in self!.records {
-                    myChanges.append((record, .Init))
+                for result in self!.results {
+                    myChanges.append((RecordModel(from: result), .Init))
                 }
             case .update(_, let deletions, let insertions, let modifications):
-                for deletion in self!.reverce(deletions) {
-                    let target = self!.records[deletion]
-                    self!.records.remove(at: deletion)
-                    
-                    myChanges.append((target, .Delete))
+                for deletion in deletions {
+                    let result = self!.results[deletion]
+                    myChanges.append((RecordModel(from: result), .Delete))
                 }
-                for insertion in self!.sort(insertions) {
+                for insertion in insertions {
                     let result = self!.results[insertion]
                     let record = RecordModel(from: result)
-                    self!.records.insert(record, at: insertion)
                     myChanges.append((record, .Insert))
                 }
                 for modification in modifications {
                     let result = self!.results[modification]
-                    self!.records[modification] = RecordModel(from: result)
-                    myChanges.append((self!.records[modification], .Modify))
+                    myChanges.append((RecordModel(from: result), .Modify))
                 }
                 
             case .error(let error):
@@ -73,20 +64,7 @@ public class RecordObserverRealm: RecordObserver {
         invalidate()
     }
     
-    func sort(_ array: [Int]) -> [Int] {
-        var temp = array
-        temp.sort(by: {$0 < $1})
-        return temp
-    }
-    
-    func reverce(_ array: [Int]) -> [Int] {
-        var temp = array
-        temp.sort(by: {$1 < $0})
-        return temp
-    }
-    
     public func invalidate() {
-        records = []
         notificationToken?.invalidate()
     }
 }
